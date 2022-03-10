2bca66632e33124967202c47b49a1382b94ef499
Larouimohammed <59184857+Larouimohammed@users.noreply.github.com>
2022-03-10 01:52:33 +0100

    Update location.py

--- location.py
+++ location.py
@@ -1,4 +1,6 @@
 from geopy.geocoders import Nominatim
+from flask import Flask, render_template
+import folium
 import smtplib,ssl
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
@@ -14,3 +16,15 @@ with smtplib.SMTP(smtp_server, port) as server:
     server.starttls(context=context)
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email,adr)
+m = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
+marker = folium.Marker(
+    location=['33.76326745','-84.39511726814364'],popup=adr)
+marker.add_to(m)
+
+m.save('templates/map.html')
+app = Flask(__name__)
+@app.route("/")
+def maplocation():
+    return render_template("map.html")
+if __name__ == '__main__':
+    app.run()
