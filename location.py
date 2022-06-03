742de232ca742a1e28cd0e2c431f7ff48f0e5ca2
Larouimohammed <59184857+Larouimohammed@users.noreply.github.com>
2022-06-03 03:45:20 +0100

    Update location.py

--- location.py
+++ location.py
@@ -1,7 +1,24 @@
+import paho.mqtt.client as mqtt
 from geopy.geocoders import Nominatim
 from flask import Flask, render_template
 import folium
 import smtplib,ssl
+
+def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
+    client.subscribe("digitest/test1")  # Subscribe to the topic “digitest/test1”, receive any messages published on it
+
+
+def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
+
+
+client = mqtt.Client("digi_mqtt_test")  # Create instance of client with client ID “digi_mqtt_test”
+client.on_connect = on_connect  # Define callback function for successful connection
+client.on_message = on_message  # Define callback function for receipt of a message
+client.connect('127.0.0.1',17300,60) # Connect to (broker, port, keepalive-time)
+client.loop_forever()  # Start networking daemon
+
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "m.laroui@esi-sba.dz"
@@ -13,9 +30,10 @@ adr=location.address
 print(adr)
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
-    server.starttls(context=context)
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email,adr)
+ server.starttls(context=context)
+#server.login(sender_email, password)
+#server.sendmail(sender_email, receiver_email,adr)
+
 m = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
 marker = folium.Marker(
     location=['33.76326745','-84.39511726814364'],popup=adr)
@@ -23,8 +41,13 @@ marker.add_to(m)
 
 m.save('templates/map.html')
 app = Flask(__name__)
-@app.route("/")
+@app.route('/')
+def onclick():
+ return render_template("button.html")
+
+@app.route('/maplocation')
 def maplocation():
-    return render_template("map.html")
+  return render_template("map.html")
+
 if __name__ == '__main__':
-    app.run()
+ app.run()
